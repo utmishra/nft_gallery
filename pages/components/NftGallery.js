f6613f9ba521842c086486a8b131f97d5fd18ce5
Utkarsh Mishra <utmishra@gmail.com>
2021-06-11 11:17:18 +0200

    Social media icons

--- pages/components/NftGallery.js
+++ pages/components/NftGallery.js
@@ -1,27 +1,62 @@
 import styles from '../../styles/Gallery.module.css';
 
 import Image from 'next/image';
-import Link from 'next/link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Grid, Box, Modal } from '@material-ui/core';
+import { useEffect, useState, useRef } from 'react';
+import SingleNft from './SingleNft';
 
-export default function NftGallery(props) {  
+const useStyles = makeStyles({
+  root: {
+    position: 'relative',
+  },
+  backdrop: {
+    position: 'relative',
+    background: 'none',
+    filter: 'blur'
+  },
+});
+
+export default function NftGallery(props) {
+  const [ isModalOpen, setModalOpen ] = useState(false);
+  const [ tokenId, setTokenId ] = useState(null);
+  const contactAddress = useRef(props.contactAddress);
+  const nft = useRef(null);
+
+  useEffect(() => {
+    if(tokenId != null) {
+      nft.current = 
+      setModalOpen(true);
+    }
+  }, [tokenId])
+
+  const handleClose = () => {
+    setModalOpen(false);
+    setTokenId(null);
+  }
+
+  // const modalBody = (props) => (
+  //   <SingleNft data={props.nft} contactAddress={contactAddress.current} tokenId={tokenId} />
+  // )
+
+  const classes = useStyles();
   
-  return (
-    <Grid container direction="row" justify="space-evenly">
+  return (    
+    <Grid container direction="row" justify="space-between">
       {
         props.nfts.map((nft) => {
           return (
-            <Grid key={nft.token_id} item xs={6}>
-              <Box className={styles['single-image']}>
-                <Link href={`/nfts/${nft.asset_contract.address}/${nft.token_id}`}>
-                <a>
-                  <Image width={182.7} height={182.7} src={nft.image_thumbnail_url}/>
-                </a>
-              </Link>
+            <Grid key={nft.token_id} item xs={12} sm={6} lg={2}>
+              <Box onClick={() => setTokenId(nft.token_id)} className={styles['single-image']}>
+                <Image width={182.7} height={182.7} src={nft.image_thumbnail_url}/>
               </Box>
-            </Grid>        
+              <Modal
+                open={isModalOpen}
+                onClose={handleClose}
+                disablePortal
+              >
+                <SingleNft data={nft} />
+              </Modal>
+            </Grid>     
           )
         })
       }
--- pages/nfts/[authorId].js
+++ pages/nfts/[authorId].js
@@ -1,8 +1,9 @@
 import styles from '../../styles/Home.module.css';
 
 import { useRouter } from 'next/router';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Container, Grid, Box } from '@material-ui/core';
 
 import useNfts from '../../lib/useNfts';
 import NftGallery from '../components/NftGallery';
@@ -33,14 +34,65 @@ export default function Gallery() {
     )
   }
 
-    return (
-      <Container className={styles.container} maxWidth="xs">
-        <Grid container>
-          <Grid item xs={12}>
-            <Owner className={styles['owner-name']} owner={data.assets[0].owner} />
-          </Grid>
+  const mediaData = [
+    {
+      icon: 'instagram.png',
+      url: 'https://www.instagram.com/elliepritts/'
+    },
+    {
+      icon: 'tiktok.svg',
+      url: 'https://www.tiktok.com/@elliepritts'
+    },
+    {
+      icon: 'twitter.png',
+      url: 'https://twitter.com/elliepritts'
+    },
+    {
+      icon: 'snapchat.svg',
+      url: 'https://www.snapchat.com/add/elliepritts'
+    },
+    {
+      icon: 'vimeo.png',
+      url: 'https://vimeo.com/elliepritts'
+    },
+    {
+      icon: 'youtube.svg',
+      url: 'https://www.youtube.com/channel/UC5AGFmvVder22_TRLrQorCg'
+    },
+    {
+      icon: 'linkedin.svg',
+      url: 'https://www.linkedin.com/in/elliepritts/'
+    }
+  ]
+
+  console.log(mediaData);
+
+  return (
+    <Container className={styles.container} maxWidth="lg">
+      <Grid container>
+        <Grid item xs={12}>
+          <Owner className={styles['owner-name']} owner={data.assets[0].owner} />
         </Grid>
-        <NftGallery authorId={authorId} nfts={data.assets} />
-      </Container>
-    )
+      </Grid>
+      <Box className={styles.icons}>
+        <Grid classes={{ label: 'icon-box' }} container direction="row" justify="center">          
+          {
+            mediaData.map((media) => {
+              console.log(media.url)
+              return (
+                <Grid key={media.icon} className={styles['single-icon']} item>
+                  <Link href={media.url}>
+                    <a target="_blank">
+                      <Image src={`/assets/images/${media.icon}`} height={34.65} width={34.65} />
+                    </a>
+                  </Link>
+                </Grid>
+              )                
+            })
+          }
+        </Grid>
+      </Box>      
+      <NftGallery authorId={authorId} nfts={data.assets} contactAddress={data.assets[0].asset_contract.address} />
+    </Container>
+  )
 }
